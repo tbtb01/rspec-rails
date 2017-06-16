3bc329df02e12865d9253b3d957297fd56e1829b
Sam Phippen <samphippen@googlemail.com>
2017-06-16 16:07:17 -0400

    Add cukes

--- example_app_generator/generate_app.rb
+++ example_app_generator/generate_app.rb
@@ -20,6 +20,7 @@ in_root do
   gsub_file 'Gemfile', /^.*\bgem 'rails.*$/, ''
   gsub_file "Gemfile", /.*web-console.*/, ''
   gsub_file "Gemfile", /.*debugger.*/, ''
+  gsub_file "Gemfile", /.*puma.*/, ""
 
   if Rails::VERSION::STRING >= '5.0.0'
     append_to_file('Gemfile', "gem 'rails-controller-testing', :git => 'https://github.com/rails/rails-controller-testing'\n")
